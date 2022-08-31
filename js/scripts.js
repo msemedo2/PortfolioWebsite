ce0a3bda41a3ad7ee561d7961b7e21623caf7508
unknown <miguel.semedo.2@outlook.com>
2022-08-31 12:33:59 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -44,3 +44,79 @@ function closeNav() {
 	}
 }
 window.addEventListener('resize', closeNav);
+
+// FORM VALIDATION
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const text = document.getElementById('text');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	validateInputs();
+
+	// let success = document.getElementsByClassName('success').length;
+	// if (success === 4) {
+	// 	sendEmail();
+	// }
+});
+
+const setError = (element, message) => {
+	const inputControl = element.parentElement;
+	const errorDisplay = inputControl.querySelector('.error');
+
+	errorDisplay.innerText = message;
+	inputControl.classList.add('error');
+	inputControl.classList.remove('success');
+};
+
+const setSuccess = (element) => {
+	const inputControl = element.parentElement;
+	const errorDisplay = inputControl.querySelector('.error');
+
+	errorDisplay.innerText = '';
+	inputControl.classList.add('success');
+	inputControl.classList.remove('error');
+};
+
+const isValidEmail = (email) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+	const nameValue = name.value.trim();
+	const emailValue = email.value.trim();
+	const subjectValue = subject.value.trim();
+	const textValue = text.value.trim();
+
+	if (nameValue === '') {
+		setError(name, 'Username is required');
+	} else {
+		setSuccess(name);
+	}
+
+	if (emailValue === '') {
+		setError(email, 'Email is required');
+	} else if (!isValidEmail(emailValue)) {
+		setError(email, 'Provide a valid email address');
+	} else {
+		setSuccess(email);
+	}
+
+	if (subjectValue === '') {
+		setError(subject, 'Subject is required');
+	} else {
+		setSuccess(subject);
+	}
+
+	if (textValue === '') {
+		setError(text, 'Please write a message');
+	} else {
+		setSuccess(text);
+	}
+};
